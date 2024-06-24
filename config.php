f7c62fb8d334190e31f5efee68ed27308e18b76f
agz-dev <agustintaverna.dev@outlook.com>
2024-06-23 21:08:10 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_NAME', 'questapi');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'questapp');
+define('TIME_ZONE', date_default_timezone_set('America/Argentina/Buenos_Aires'));
